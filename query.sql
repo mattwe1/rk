1a0e39cc85c4e98ca5062dc9e6ba96c854c1f747
Matvett <matant0775@gmail.com>
2024-12-20 19:28:29 +0300

    Add sql query to filter IT employees

---
+++ query.sql
@@ -0,0 +1,6 @@
+git init
+git add query.sql
+git commit -m "Add SQL query to filter IT employees"
+git branch -M main
+git remote add origin https://github.com/yourusername/yourrepository.git
+git push -u origin main
